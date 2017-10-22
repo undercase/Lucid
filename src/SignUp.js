ab28abe5405f71b929be49db117cb5ae80f0e188
Justin Wei <wei.justin.a@gmail.com>
2017-10-22 04:10:31 -0500

    added registration

--- src/SignUp.js
+++ src/SignUp.js
@@ -4,6 +4,38 @@ import { Link } from 'react-router-dom';
 import './css/SignUp.css';
 
 export default class SignUp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: '',
+      password: '',
+      confirmpassword: ''
+    }
+  }
+
+  handleSubmit = (e) => {
+    var registration = {
+      method: 'POST',
+      body: JSON.stringify({      name:this.state.user,
+            password:this.state.password
+          })
+    };
+
+    fetch('/register', registration);
+  }
+
+  handleUserChange = (event) => {
+    this.setState({user: event.target.value});
+  }
+
+  handlePassChange = (event) => {
+    this.setState({password: event.target.value});
+  }
+
+  handleConfirmPassChange = (event) => {
+    this.setState({confirmpassword: event.target.value});
+  }
+
   render() {
     return (
       <div className="signup">
@@ -15,10 +47,10 @@ export default class SignUp extends Component {
           </div>
           <div className="right">
             <form>
-              <input type="text" placeholder="Email" />
-              <input type="password" placeholder="Password" />
-              <input type="password" placeholder="Confirm Password" />
-              <div className="ghost button">Sign Up</div>
+              <input value={this.state.user} type="text" placeholder="Email" onChange={this.handleUserChange}/>
+              <input value={this.state.password} type="password" placeholder="Password" onChange={this.handlePassChange}/>
+              <input value={this.state.confirmpassword} type="password" placeholder="Confirm Password" onChange={this.handleConfirmPassChange}/>
+              <div className="ghost button" onClick={this.handleSubmit}>Sign Up</div>
             </form>
           </div>
         </div>
@@ -26,4 +58,3 @@ export default class SignUp extends Component {
     );
   }
 }
-
