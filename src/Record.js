0a760accd918ba2a7cfb92061638fb4f32f8dd53
Jason Park <parkjs814@gmail.com>
2017-10-22 09:10:31 -0400

    Add a graph with dummy data

--- src/Record.js
+++ src/Record.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import SpeechRecognition from 'react-speech-recognition';
+import { Line } from 'react-chartjs-2';
 
 import './css/Record.css';
 
@@ -20,7 +21,7 @@ class Record extends Component {
       method: 'GET',
       credentials: 'same-origin',
     }).then(res => res.json()).then(dreams => {
-      let card_arr = Array.from(dreams).map(function(dream, i) {
+      let card_arr = Array.from(dreams).map(function (dream, i) {
         return <Card date={dream.date} text={dream.text} sentiment={dream.sentiment} key={i} />
       })
       console.log(card_arr);
@@ -49,8 +50,8 @@ class Record extends Component {
         'Content-Type': 'application/json'
       },
       credentials: 'same-origin',
-      
-      body:JSON.stringify({text: dreamtext})
+
+      body: JSON.stringify({ text: dreamtext })
     })
   }
 
@@ -130,6 +131,40 @@ class Record extends Component {
                 night.
               </div>
           }
+          <div>
+            <Line data={{
+              labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+              datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                  'rgba(255,99,132,1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+              }]
+            }} options={{
+              scales: {
+                yAxes: [{
+                  ticks: {
+                    beginAtZero: true
+                  }
+                }]
+              }
+            }} width="600" height="250" />
+          </div>
           {this.state.dreams}
         </div>
       </div>
