2ff599a6bd0674698de9be3d094de2e0797e4d1b
Justin Wei <wei.justin.a@gmail.com>
2017-10-22 04:39:44 -0500

    added sign in

--- src/SignIn.js
+++ src/SignIn.js
@@ -4,6 +4,42 @@ import { Link } from 'react-router-dom';
 import './css/SignIn.css';
 
 export default class SignIn extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: '',
+      password: '',
+      confirmpassword: ''
+    }
+  }
+
+
+  handleUserChange = (event) => {
+    this.setState({user: event.target.value});
+  }
+
+  handlePassChange = (event) => {
+    this.setState({password: event.target.value});
+  }
+
+  handleSubmit = (e) => {
+    console.log(this.state.user);
+    console.log(this.state.password);
+    var registration = {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+
+      body: JSON.stringify({
+            name:this.state.user,
+            password:this.state.password
+          })
+    };
+    fetch('/login', registration);
+  }
+
   render() {
     return (
       <div className="signin">
@@ -11,12 +47,11 @@ export default class SignIn extends Component {
           <h1>Lucid</h1>
         </Link>
         <form>
-          <input type="text" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <div className="ghost button">Submit</div>
+          <input value={this.state.user} type="text" placeholder="Email" onChange={this.handleUserChange}/>
+          <input value={this.state.password} type="password" placeholder="Password" onChange={this.handlePassChange}/>
+          <div className="ghost button" onClick={this.handleSubmit}>Sign In</div>
         </form>
       </div>
     );
   }
 }
-
